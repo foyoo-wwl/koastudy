b154f7646a6b527263e8d607f479f8c2dcd8836c
foyo <411404821@qq.com>
2020-05-10 13:04:02 +0800

    feat: 路由的演示

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,19 +1,41 @@
 const router = require('koa-router')()
 
 router.get('/', async (ctx, next) => {
-  await ctx.render('index', {
-    title: 'Hello Koa 2!'
-  })
+    await ctx.render('index', {
+        title: 'Hello Koa 2!'
+    })
 })
 
 router.get('/string', async (ctx, next) => {
-  ctx.body = 'koa2 string'
+    ctx.body = 'koa2 string'
 })
 
 router.get('/json', async (ctx, next) => {
-  ctx.body = {
-    title: 'koa2 json'
-  }
+    ctx.body = {
+        title: 'koa2 json'
+    }
+})
+
+router.get("/profile/:username", async (ctx, next) => {
+    const {
+        username
+    } = ctx.params
+    ctx.body = {
+        tittle: "this is",
+        name: username
+    }
+})
+
+router.get("/profile/:username/:pageIndex", async (ctx, next) => {
+    const {
+        username,
+        pageIndex
+    } = ctx.params
+    ctx.body = {
+        tittle: "this is",
+        name: username,
+        page: pageIndex
+    }
 })
 
 module.exports = router
